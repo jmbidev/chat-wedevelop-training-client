6f2b9fd6642720cd12af562667200af229d83b65
Nahuel Dallacamina <nahudalla@gmail.com>
2019-05-28 09:11:50 -0300

    Added error message alert to AuthPageFormat

--- src/modules/Authentication/components/AuthPageFormat/index.js
+++ src/modules/Authentication/components/AuthPageFormat/index.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Alert from 'react-bootstrap/Alert'
 import PropTypes from 'prop-types'
 
 export default AuthPageFormat
 
-function AuthPageFormat ({ title, link, children }) {
+function AuthPageFormat ({ title, link, children, showErrorMessage }) {
+  const [errorMessageHidden, setErrorMessageHidden] = useState(false)
+
   return (
     <Container>
       <Row className='justify-content-center'>
         <Col lg='5' md='7' sm='9' xs='12'>
           <h3 className='my-5 text-center'>{title}</h3>
+          { showErrorMessage && (
+            <Alert
+              dismissible
+              variant='danger'
+              show={!errorMessageHidden}
+              onClose={() => setErrorMessageHidden(true)}
+            >
+              <b>Oh snap!</b> Something went wrong.
+            </Alert>
+          )}
           {children}
           <Link to={link.to} className='w-100 d-block text-center'>{link.text}</Link>
         </Col>
@@ -27,5 +40,6 @@ AuthPageFormat.propTypes = {
   link: PropTypes.exact({
     to: PropTypes.string.isRequired,
     text: PropTypes.node.isRequired
-  })
+  }),
+  showErrorMessage: PropTypes.bool
 }
