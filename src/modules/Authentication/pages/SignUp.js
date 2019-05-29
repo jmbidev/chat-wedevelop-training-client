0bff662be3fb5318ba6acbb0f8880a1721734a5f
Nahuel Dallacamina <nahudalla@gmail.com>
2019-05-29 12:23:11 -0300

    Manage error message state. Use success and error callbacks from SignUpForm

--- src/modules/Authentication/pages/SignUp.js
+++ src/modules/Authentication/pages/SignUp.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import AuthPageFormat from '../components/AuthPageFormat'
 import SignUpForm from '../components/SignUpForm'
 
 export default (props) => {
+  const [showErrorMessage, setShowErrorMessage] = useState(false)
+
   return (
     <AuthPageFormat
       title='Member registration'
       link={{ to: '/auth/login', text: 'Do you already have an account?' }}
+      showErrorMessage={showErrorMessage}
+      onHideErrorMessage={() => setShowErrorMessage(false)}
     >
-      <SignUpForm onSubmit={handleFormSubmit} />
+      <SignUpForm
+        onSuccess={handleSuccess}
+        onError={handleError}
+      />
     </AuthPageFormat>
   )
 
-  function handleFormSubmit (fields) {
-    console.log(fields)
+  function handleSuccess (user) {
+    console.log(user)
+    setShowErrorMessage(false)
+  }
+
+  function handleError (error) {
+    console.error(error)
+    setShowErrorMessage(true)
   }
 }
